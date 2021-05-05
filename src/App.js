478b365f7e5089ce544a85eb19b31b57f5163339
Neelesh Jaiswal <neelesh.jaiswal@yash.com>
2021-05-06 00:20:19 +0530

    feat: added css and bootstrap cdn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Learn React Testing</h1>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Jotto</h1>
+            <Congrats success={true}/>
+            <GuessedWords guessedWords={[
+                {guessedWord: 'train', letterMatchCount: 3},
+                {guessedWord: 'agile', letterMatchCount: 1},
+                {guessedWord: 'party', letterMatchCount: 5}
+            ]}/>
+        </div>
+    );
 }
 
 export default App;
--- src/Congrats.js
+++ src/Congrats.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Congrats({success}) {
 
     if (success) {
-        return (<div data-test={'component-congrats'}>
+        return (<div data-test={'component-congrats'} className={'alert alert-success'}>
               <span data-test={'component-message'}>
                   Congratulations! You guessed the word!
               </span>
